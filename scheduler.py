eb953ac250b2ef02646335a0920f16ff24972fe0
Jakob Jenko <jakob.jenko@xlab.si>
2023-06-21 14:26:59 +0200

    First version

---
+++ scheduler.py
@@ -0,0 +1,10 @@
+from utils.datafetcher import *
+import pandas as pd
+url = 'http://localhost:5689/forecast/renewable_potential'
+lat = 14
+lon = 46
+
+fetcher = DataFetcher(url, lat, lon)
+data = fetcher.fetch_data()
+
+print(data.)
