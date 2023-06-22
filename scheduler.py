5d606eb26b02f970734de976b2d97e09f7bf24ac
Jakob Jenko <jakob.jenko@xlab.si>
2023-06-22 15:59:49 +0200

    First version

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,30 @@
+from datetime import datetime, timedelta
+
+import pandas as pd
+
 from utils.datafetcher import DataFetcher
 
 url = "http://localhost:5689/forecast/renewable_potential"
 lat = 14
 lon = 46
-
-fetcher = DataFetcher(url, lat, lon)
+runtime_hours = 2
+deadline_hours = 48
+location = {"lat": 40.7128, "lon": -74.0060}
+fetcher = DataFetcher(url, "Ljubljana")
 data = fetcher.fetch_data()
+# data.date = pd.to_datetime(data.index, unit='s')
 
-print(data)
+res = data.resample(str(runtime_hours) + "H").mean()
+res = res.renewable_potential_forecast_hourly.sort_values(ascending=False)
+
+current_date = pd.Timestamp(datetime.now())
+deadline_date = pd.Timestamp(datetime.now() + timedelta(hours=deadline_hours))
+filtered_res = res[(res.index > current_date) & (res.index < deadline_date)]
+
+for i in range(10):
+    print(
+        "Optimal time between ",
+        filtered_res.index[i],
+        "and",
+        filtered_res.index[i] + timedelta(hours=runtime_hours),
+    )
