a42391a79b144a3a1679d0bd9310ab7834dc27f9
Jakob Jenko <jakob.jenko@xlab.si>
2023-06-30 00:30:48 +0200

    Deadline is now time by when script has to finish exectuing

--- renops/scheduler.py
+++ renops/scheduler.py
@@ -24,6 +24,7 @@ def execute_script(script_path):
 
 
 def main():
+    print("RUNNING RENOPS SCHEDULER...")
     parser = argparse.ArgumentParser()
     parser.add_argument("script_path", help="Path to the script to be executed.")
     parser.add_argument(
@@ -33,43 +34,62 @@ def main():
         help="Location in string - 'settlement,country' i.e 'Berlin, Germany')",
     )
     parser.add_argument(
-        "-r", "--runtime", type=int, default=3, help="Runtime in hours."
+        "-r", "--runtime", type=int, default=None, help="Runtime in hours."
     )
     parser.add_argument(
-        "-d", "--deadline", type=int, default=120, help="Deadline in hours."
+        "-d",
+        "--deadline",
+        type=int,
+        default=120,
+        help="Deadline in hours, by when should script finish running",
     )
     parser.add_argument("-v", "--verbose", type=int, default=None, help="Verbose mode.")
 
     args = parser.parse_args()
+
+    if not args.runtime:
+        print("Runtime not specified, using default setting of 3 hours!")
+        args.runtime = 3
+
     url = "https://renops-api-tango.xlab.si/forecast/renewable_potential"
     fetcher = DataFetcher(url, location=args.location)
     data = fetcher.fetch_data()
 
     res = data.resample(str(args.runtime) + "H").mean()
-    res = res.renewable_potential_forecast_hourly.sort_values(ascending=False)
+    res = res.renewable_potential_forecast_hourly
+
+    renewables_now = res.values[0]
+    res = res.sort_values(ascending=False)
 
     current_date = pd.Timestamp(datetime.now())
     deadline_date = pd.Timestamp(datetime.now() + timedelta(hours=args.deadline))
-    filtered_res = res[(res.index >= current_date) & (res.index < deadline_date)]
+    start_execution_date = pd.Timestamp(deadline_date - timedelta(hours=args.runtime))
 
+    print("Task has to be finished by: ", deadline_date)
+    filtered_res = res[
+        (res.index >= current_date) & (res.index <= start_execution_date)
+    ]
+    filtered_res = filtered_res[res != 0]
     # Here you would call the function to get the optimal time, using the location, runtime and deadline
     # For now, let's just use the deadline time.
 
     if len(filtered_res) <= 1:
         optimal_time = datetime.now()
-        filtered_res[pd.Timestamp(datetime.now())] = None
+        filtered_res[pd.Timestamp(datetime.now())] = renewables_now
+        print("No renewable window whitin given deadline!")
+        print(f"Current renewable potential is: {renewables_now}")
     else:
         optimal_time = filtered_res.index[0]
 
-    print(
-        "Found optimal time between ",
-        filtered_res.index[0],
-        "and",
-        filtered_res.index[0] + timedelta(hours=args.runtime),
-    )
-    print("Renewable potential at that time is:", filtered_res[0].round(2))
+        print(
+            "Found optimal time between ",
+            filtered_res.index[0],
+            "and",
+            filtered_res.index[0] + timedelta(hours=args.runtime),
+        )
+        print("Renewable potential at that time is:", filtered_res[0].round(2))
+        print(f"Waiting until {optimal_time} to execute {args.script_path}...")
 
-    print(f"Waiting until {optimal_time} to execute {args.script_path}...")
     wait_until(optimal_time)
     print(f"Executing {args.script_path} now at {datetime.now()}")
     print("----------------------------------------------------")
