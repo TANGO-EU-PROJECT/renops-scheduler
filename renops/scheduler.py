225340e61a1d3f3972be6d69841b35b35afc1694
Jakob Jenko <jakob.jenko@xlab.si>
2023-09-08 13:06:46 +0200

    Improve help description for location

--- renops/scheduler.py
+++ renops/scheduler.py
@@ -42,13 +42,22 @@ def to_datetime(epoch):
 
 def main():
     print("RUNNING RENOPS SCHEDULER...")
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument("script_path", help="Path to the script to be executed.")
     parser.add_argument(
         "-l",
         "--location",
         default=None,
-        help="Location in string - 'settlement,country' i.e 'Berlin, Germany')",
+        help=(
+        'Location can be specified in two ways:\n\n'
+        '1. Pass a specific location as a string, e.g., "Berlin, Germany".\n\n'
+        '2. Use automatic location detection based on IP address.\n'
+        ' By using this tag, you agree that your IP can be used to detect your location.\n'
+        'You can use any of the following values for this purpose:\n'
+        '   -l a (-la)\n'
+        '   -l auto\n'
+        '   -l automatic\n'            
+            ),
         required=True
     )
     parser.add_argument(
